224d40e317d2cc7bb6d35f514a9e8628ec77cde3
Frostela <mainakdashlr@gmail.com>
2024-10-01 15:38:18 +0530

    berger manu fixed, scroll animation fixed, pages added

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const manubar = document.getElementById('manubar');
-    if (manubar) {
-        manubar.addEventListener('click', function() {
-            const menu = document.getElementById('menu');
-            menu.classList.toggle('expanded'); // Add/remove the expanded class
-        });
-    } else {
-        console.error("Manubar element not found");
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const manubar = document.getElementById("manubar");
+  if (manubar) {
+    manubar.addEventListener("click", function () {
+      const menu = document.getElementById("menu");
+      menu.classList.toggle("expanded"); // Add/remove the expanded class
+    });
+  } else {
+    console.error("Manubar element not found");
+  }
+
+  
+  
 });
