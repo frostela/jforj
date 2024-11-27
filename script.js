18a8275cad68575bbb0de1aa32e03121d805cb2f
Frostela <mainakdashlr@gmail.com>
2024-11-27 19:51:55 +0530

    reseting lm scroll

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-  });  
-
-  // Trigger update when the footer is loaded or when the layout changes
-  scroll.update();
-
   const manubar = document.getElementById("manubar");
   if (manubar) {
     manubar.addEventListener("click", function () {
